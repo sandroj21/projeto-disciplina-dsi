57d27e789564d2059e5642f1157087a1ba1935c2
Sandro Junior <58084207+sandroj21@users.noreply.github.com>
2023-09-29 12:04:36 -0300

    atualizando

---
+++ demo/src/main/java/com/example/demo/entities/Curso.java
@@ -0,0 +1,26 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Curso {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nome;
+
+    @Override
+    public String toString() {
+        return "Curso [id=" + id + ", nome=" + nome +"]";
+    }
+}
---
+++ demo/src/main/java/com/example/demo/services/CursoService.java
@@ -0,0 +1,30 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Curso;
+import com.example.demo.repositories.CursoRepository;
+
+@Service
+public class CursoService {
+    
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public Curso getCurso(Integer idCurso) {
+        return cursoRepository.getReferenceById(idCurso);
+    }
+
+    public Curso salvar(Curso curso) {
+        return cursoRepository.save(curso);
+    }
+
+    public Curso atualizar(Curso curso) {
+        return cursoRepository.save(curso);
+    }
+    
+    public void remover(Integer idCurso) {
+        cursoRepository.deleteById(idCurso);
+    }
+}
